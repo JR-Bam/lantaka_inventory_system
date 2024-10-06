bf33b2a731062e85bc687503b20b28affbf8bd0a
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2024-10-06 22:20:48 +0800

    Added simple server to serve the files

--- lantaka_inventory_system.cpp
+++ lantaka_inventory_system.cpp
@@ -2,10 +2,25 @@
 //
 
 #include <iostream>
+#include <httplib.h>
+
+using namespace httplib;
+
+void helloworld(const Request& req, Response& res) {
+    res.set_content("Hello, World!", "text/plain");
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Server svr;
+
+    svr.set_base_dir("./public");
+
+    svr.Get("/home/this", &helloworld);
+
+    std::cout << "Starting server..." << std::endl;
+    svr.listen("localhost", 8080);
+
     return 0;
 }
 
