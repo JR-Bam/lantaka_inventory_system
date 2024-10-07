142faa39f809546d985660bbe85be5a07f227fb5
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2024-10-07 20:28:00 +0800

    updated frontend and made simple account verification

--- lantaka_inventory_system.cpp
+++ lantaka_inventory_system.cpp
@@ -2,21 +2,57 @@
 //
 
 #include <iostream>
-#include <httplib.h>
+#include "json.hpp"
+#include "httplib.h"
 
 using namespace httplib;
+using json = nlohmann::json;
 
-void helloworld(const Request& req, Response& res) {
-    res.set_content("Hello, World!", "text/plain");
+const std::string dummyUsername = "admin";
+const std::string dummyPassword = "password";
+
+void verifyAccount(const Request& req, Response& res) {
+    try{
+        json req_json = json::parse(req.body);
+        std::string username = req_json.at("username");
+        std::string password = req_json.at("password");
+
+        json res_body;
+
+        // In a real life context the password should be hashed/encypted before being sent here
+        // "But this is already a real life context" I do not care
+        if (username == dummyUsername && password == dummyPassword) {
+            res_body["status"] = "success";
+            res.status = 200; // HTTP 200 OK
+        } else {
+            res_body["status"] = "error";
+            res_body["errorType"] = "UNAUTHORIZED_LOGIN";
+            res.status = 401; // HTTP 401 Unauthorized
+        }
+
+        res.set_content(res_body.dump(), "application/json");
+
+    } catch (const std::exception&){
+        json response_json;
+        response_json["status"] = "error";
+        response_json["errorType"] = "BAD_REQUEST";
+
+        res.set_content(response_json.dump(), "application/json");
+        res.status = 400;  // HTTP 400 Bad Request
+    }
 }
 
 int main()
 {
     Server svr;
 
-    svr.set_base_dir("./public");
+    // For HTML Files
+    svr.set_mount_point("/", "./public");
+    // For the images
+    svr.set_mount_point("/images", "./assets");
 
-    svr.Get("/home/this", &helloworld);
+    // Routes
+    svr.Post("/api/verify", &verifyAccount);
 
     std::cout << "Starting server..." << std::endl;
     svr.listen("localhost", 8080);
