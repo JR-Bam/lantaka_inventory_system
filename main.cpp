52aa84a69554ca8897f7c6eee404f86a360eb53e
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2024-10-09 23:51:34 +0800

    added mysql library

--- main.cpp
+++ main.cpp
@@ -6,14 +6,47 @@
 #include "RouteHandlers.h"
 
 #include <iostream>
+#include "mysqlx/xdevapi.h"
 
 void hello(const Request& req, Response& res) {
     res.set_content("Hello, World!", "text/plain");
 }
 
+
+// Testing out the mysql library. This is just to print all the databases within my mysql server
+void testMySQL() {
+    using namespace mysqlx;
+    try
+    {
+        Session sesh(
+            SessionOption::HOST, "127.0.0.1",
+            SessionOption::PORT, 33060,
+            SessionOption::USER, "app",
+            SessionOption::PWD, ""
+        ); // Change these to fit your own mysql server
+
+        for (auto schema : sesh.getSchemas()) {
+            std::cout << "Database name: " << schema.getName() << std::endl;
+        }
+
+        sesh.close();
+    }
+    catch (const mysqlx::Error& err) {
+        std::cerr << "Error: " << err.what() << '\n';
+        exit(1);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        exit(1);
+    }
+
+}
+
 int main()
 {
     Server svr;
+
+    testMySQL();
 
     // For HTML Files
     svr.set_mount_point("/", "./public");
